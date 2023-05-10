5cbe9065f4e5c304deb427fdc68f17e53fe87a40
tomoya <cj.0s0ttw.nsm4@gmail.com>
2023-05-10 11:20:23 +0000

    試合詳細データ取得・成形

--- backend/app/index.py
+++ backend/app/index.py
@@ -52,7 +52,7 @@ def match():
 
 
 def get_match_detail(match_ID, gameNo, isPoint, player):
-    query = f"SELECT (game_number,pointmiss_order,serve,shot_type,fore_back,course,poach_volley_course,miss_result,rally_count)FROM public.points_misses JOIN game_players ON public.points_misses.match_id = public.game_players.match_id WHERE public.points_misses.match_id = {match_ID} AND public.points_misses.game_number = {gameNo} AND public.points_misses.ispoint = {isPoint} AND public.game_players.player_number = {player}  ORDER BY public.points_misses.pointmiss_order ASC"
+    query = f"SELECT (game_number,pointmiss_order,serve,shot_type,fore_back,course,poach_volley_course,miss_result,rally_count) FROM public.points_misses WHERE public.points_misses.match_id = {match_ID} AND public.points_misses.game_number = {gameNo} AND public.points_misses.ispoint = {isPoint} AND public.points_misses.player_id = (SELECT player_id FROM public.game_players WHERE match_id = {match_ID} AND player_number = '{player}') ORDER BY public.points_misses.pointmiss_order ASC "
     return query
 
 
@@ -65,14 +65,15 @@ def match_detail(match_id):
             game_number_list = df.to_dict(orient="records")
 
             array = []
-            game_object = {
-                "player1": {"ポイント": [], "ミス": []},
-                "player2": {"ポイント": [], "ミス": []},
-            }
+
             game_player_list = ["player1", "player2"]
 
             for game_number in game_number_list:
                 gameNo = game_number["game_number"]
+                game_object = {
+                    "player1": {"point": [], "miss": []},
+                    "player2": {"point": [], "miss": []},
+                }
 
                 for game_player in game_player_list:
                     df = pd.read_sql(
@@ -86,11 +87,10 @@ def match_detail(match_id):
                     )
                     miss_list = df.to_dict(orient="records")
 
-                    game_object[game_player]["ポイント"].append(point_list)
-                    game_object[game_player]["ミス"].append(miss_list)
-                    array.append(game_object)
+                    game_object[game_player]["point"] = point_list
+                    game_object[game_player]["miss"] = miss_list
 
-        print(game_bject)
+                array.append(game_object)
 
     return array
 
